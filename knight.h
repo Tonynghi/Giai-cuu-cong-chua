a5b611e8827292d1754a1f3a4dbac50c7b6ddd18
Tonynghi <115861830+Tonynghi@users.noreply.github.com>
2023-03-04 09:29:23 +0700

    Add files via upload

---
+++ knight.h
@@ -0,0 +1,17 @@
+#ifndef __KNIGHT_H__
+#define __KNIGHT_H__
+
+#include "main.h"
+
+int MG3(int arr[100], int size);
+int MG4(int arr[100], int size);
+int sumMaMi(int a[100], int asize);
+int sumn(int n);
+bool isPrime(int n);
+int nearestPrime(int anc);
+int nearestFibo(int n);
+
+void display(int HP, int level, int remedy, int maidenkiss, int phoenixdown, int rescue);
+void adventureToKoopa(string file_input, int & HP, int & level, int & remedy, int & maidenkiss, int & phoenixdown, int & rescue);
+
+#endif // __KNIGHT_H__
